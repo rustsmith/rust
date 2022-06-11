7f36505d0f6d1aa2729a819999cc1453e90c78da
Mayank Sharma <mayank.sharma.uk@gmail.com>
2022-06-11 22:49:46 +0100

    Revert change to flags

--- src/bootstrap/builder.rs
+++ src/bootstrap/builder.rs
@@ -1366,7 +1366,6 @@ impl<'a> Builder<'a> {
         if mode != Mode::Std {
             rustflags.arg("-Cinstrument-coverage");
             rustflags.arg("-Zprofile");
-            rustflags.arg("-Ztarget-applies-to-host");
         }
 
         let use_new_symbol_mangling = match self.config.rust_new_symbol_mangling {
